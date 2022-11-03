c1812fc0595b87a08ea61f794062940df529c382
root <root@localhost.localdomain>
2022-11-03 21:18:50 +0800

    Parser解析器数据结构定义

--- msgnats-server/src/parser.rs
+++ msgnats-server/src/parser.rs
@@ -52,3 +52,17 @@ pub enum ParseResult<'a> {
     SubArg(SubArg<'a>),
     PubArg(PubArg<'a>),
 }
+
+// 解析器数据结构定义
+const  DEFAULT_BUF_LEN: usize= 512; // 默认解析缓冲区大小为512
+
+pub struct Parser {
+    state: ParseState,
+    buf: [u8; DEFAULT_BUF_LEN], // 默认缓冲区大小512 超过另行分配使用msg_buf
+    arg_len: usize,
+    msg_buf: Option<Vec<u8>>,
+     //解析过程中收到新消息,那么 新消息的总长度是msg_total_len,已收到部分应该是msg_len
+    msg_total_len: usize,
+    msg_len: usize,
+    debug: bool,
+}
