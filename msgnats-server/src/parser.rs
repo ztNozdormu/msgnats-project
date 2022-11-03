2896e074cf9af6d1b817f6cd884ab6782fdd8593
root <root@localhost.localdomain>
2022-11-03 21:09:13 +0800

    解析结果数据结构定义

--- msgnats-server/src/parser.rs
+++ msgnats-server/src/parser.rs
@@ -31,3 +31,24 @@ enum ParseState {
     OpMsg,  //pub message
     OpMsgFull,
 }
+
+// 解析结果定义
+#[derive(Debug,PartialEq)]
+pub struct SubArg<'a> {
+    pub subject: &'a str, // 'a str 避免内存复制
+    pub sid: &'a str,
+    pub queue: Option<&'a str>, 
+}
+#[derive(Debug,PartialEq)]
+pub struct PubArg<'a> {
+    pub subject: &'a str,
+    pub size_buf: &'a str, // str字符串切片形式避免内存复制
+    pub size: usize,
+    pub msg: &'a [u8],
+}
+#[derive(Debug,PartialEq)]
+pub enum ParseResult<'a> {
+    NoMsg,  // buf="sub top.stevenbai.blog" sub消息格式不完整
+    SubArg(SubArg<'a>),
+    PubArg(PubArg<'a>),
+}
